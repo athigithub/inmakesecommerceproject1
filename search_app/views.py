e34b3730714a6a469fdb16617c95153ac6bb3680
athigithub <athiz.inmakes@gmail.com>
2023-01-12 16:11:39 +0530

    saved

---
+++ search_app/views.py
@@ -0,0 +1,16 @@
+from django.db.models import Q
+from django.shortcuts import render
+from ecommerceapp.models import  Product
+
+
+# Create your views here.
+
+def SearchResult(request):
+    products=None
+    query=None
+    if 'q' in request.GET:
+        query=request.GET.get('q')
+        products=Product.objects.all().filter(Q(name__contains=query) | Q(description__contains=query))
+    return render(request,'search.html',{'query':query,'products':products})
+
+
